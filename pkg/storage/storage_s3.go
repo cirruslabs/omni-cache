a81bd2e027ed792eb7a58b00da22330f8a2bde58
Fedor Korotkov <fedor.korotkov@gmail.com>
2025-10-26 20:55:35 -0500

    Better API

--- pkg/storage/storage_s3.go
+++ pkg/storage/storage_s3.go
@@ -4,9 +4,11 @@ import "github.com/aws/aws-sdk-go/service/s3"
 
 type s3Storage struct {
 	BlobStorageBacked
-	client *s3.S3
+	client     *s3.S3
+	bucketName string
+	prefix     []string
 }
 
-func NewS3Storage(client *s3.S3) BlobStorageBacked {
-	return &s3Storage{client: client}
+func NewS3Storage(client *s3.S3, bucketName string, prefix ...string) BlobStorageBacked {
+	return &s3Storage{client: client, bucketName: bucketName, prefix: prefix}
 }
