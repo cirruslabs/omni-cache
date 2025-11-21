0dc6eaf3996c8f0f4c3ba2bfb1007511763dc267
Fedor Korotkov <fedor.korotkov@gmail.com>
2025-11-21 15:14:29 -0500

    Use slog

--- internal/protocols/http_cache/http_cache.go
+++ internal/protocols/http_cache/http_cache.go
@@ -4,7 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"io"
-	"log"
+	"log/slog"
 	"net/http"
 
 	"github.com/cirruslabs/omni-cache/pkg/protocols"
@@ -46,13 +46,14 @@ func (httpCache *internalHTTPCache) downloadCache(w http.ResponseWriter, r *http
 
 	infos, err := httpCache.storagBackend.DownloadURLs(r.Context(), cacheKey)
 	if err != nil {
-		log.Printf("%s cache download failed: %v\n", cacheKey, err)
-
+		slog.ErrorContext(r.Context(), "cache download failed", "cacheKey", cacheKey, "err", err)
 		w.WriteHeader(http.StatusNotFound)
-	} else {
-		log.Printf("Redirecting cache download of %s\n", cacheKey)
-		httpCache.proxyDownloadFromURLs(w, r, infos)
+
+		return
 	}
+
+	slog.InfoContext(r.Context(), "redirecting cache download", "cacheKey", cacheKey)
+	httpCache.proxyDownloadFromURLs(w, r, infos)
 }
 
 func (httpCache *internalHTTPCache) proxyDownloadFromURLs(w http.ResponseWriter, r *http.Request, infos []*storage.URLInfo) {
@@ -67,7 +68,7 @@ func (httpCache *internalHTTPCache) proxyDownloadFromURLs(w http.ResponseWriter,
 func (httpCache *internalHTTPCache) proxyDownloadFromURL(w http.ResponseWriter, r *http.Request, info *storage.URLInfo) bool {
 	req, err := http.NewRequestWithContext(r.Context(), r.Method, info.URL, nil)
 	if err != nil {
-		log.Printf("Failed to create a new GET HTTP request to URL %s: %v", info.URL, err)
+		slog.ErrorContext(r.Context(), "failed to create cache proxy request", "url", info.URL, "err", err)
 		return false
 	}
 	for k, v := range info.ExtraHeaders {
@@ -75,23 +76,23 @@ func (httpCache *internalHTTPCache) proxyDownloadFromURL(w http.ResponseWriter, 
 	}
 	resp, err := httpCache.httpClient.Do(req)
 	if err != nil {
-		log.Printf("Proxying cache %s failed: %v\n", info.URL, err)
+		slog.ErrorContext(r.Context(), "proxy cache request failed", "url", info.URL, "err", err)
 		return false
 	}
 	defer resp.Body.Close()
 	successfulStatus := 100 <= resp.StatusCode && resp.StatusCode < 300
 	if !successfulStatus {
-		log.Printf("Proxying cache %s failed with %d status\n", info.URL, resp.StatusCode)
+		slog.ErrorContext(r.Context(), "proxy cache request returned non-successful status", "url", info.URL, "statusCode", resp.StatusCode)
 		return false
 	}
 	w.WriteHeader(resp.StatusCode)
 	bytesRead, err := io.Copy(w, resp.Body)
 	if err != nil {
-		log.Printf("Proxying cache download for %s failed with %v\n", info.URL, err)
+		slog.ErrorContext(r.Context(), "proxy cache download failed", "url", info.URL, "err", err)
 		return false
 	}
 
-	log.Printf("Proxying cache %s succeded! Proxies %d bytes!\n", info.URL, bytesRead)
+	slog.InfoContext(r.Context(), "proxy cache succeeded", "url", info.URL, "bytesProxied", bytesRead)
 	return true
 }
 
@@ -101,7 +102,7 @@ func (httpCache *internalHTTPCache) uploadCacheEntry(w http.ResponseWriter, r *h
 	info, err := httpCache.storagBackend.UploadURL(r.Context(), cacheKey, nil)
 	if err != nil {
 		errorMsg := fmt.Sprintf("Failed to initialized uploading of %s cache! %s", cacheKey, err)
-		log.Println(errorMsg)
+		slog.ErrorContext(r.Context(), "failed to initialize cache upload", "cacheKey", cacheKey, "err", err)
 
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte(errorMsg))
@@ -109,7 +110,7 @@ func (httpCache *internalHTTPCache) uploadCacheEntry(w http.ResponseWriter, r *h
 	}
 	req, err := http.NewRequest("PUT", info.URL, bufio.NewReader(r.Body))
 	if err != nil {
-		log.Printf("%s cache upload failed: %v\n", cacheKey, err)
+		slog.ErrorContext(r.Context(), "cache upload request creation failed", "cacheKey", cacheKey, "uploadURL", info.URL, "err", err)
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
@@ -121,17 +122,31 @@ func (httpCache *internalHTTPCache) uploadCacheEntry(w http.ResponseWriter, r *h
 	resp, err := httpCache.httpClient.Do(req)
 	if err != nil {
 		errorMsg := fmt.Sprintf("Failed to proxy upload of %s cache! %s", cacheKey, err)
-		log.Println(errorMsg)
+		slog.ErrorContext(r.Context(), "failed to proxy cache upload", "cacheKey", cacheKey, "uploadURL", info.URL, "err", err)
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte(errorMsg))
 		return
 	}
+	defer resp.Body.Close()
+
 	if resp.StatusCode >= 400 {
-		log.Printf("Failed to proxy upload of %s cache! %s", cacheKey, resp.Status)
-		log.Printf("Headers for PUT request to  %s\n", info.URL)
-		req.Header.Write(log.Writer())
-		log.Println("Failed response:")
-		resp.Write(log.Writer())
+		slog.ErrorContext(
+			r.Context(),
+			"cache upload proxy returned error response",
+			"cacheKey", cacheKey,
+			"status", resp.Status,
+			"statusCode", resp.StatusCode,
+			"uploadURL", info.URL,
+			"requestHeaders", req.Header,
+		)
+
+		body, bodyErr := io.ReadAll(resp.Body)
+		switch {
+		case bodyErr != nil:
+			slog.ErrorContext(r.Context(), "failed to read cache upload error response body", "cacheKey", cacheKey, "uploadURL", info.URL, "err", bodyErr)
+		case len(body) > 0:
+			slog.ErrorContext(r.Context(), "cache upload error response body", "cacheKey", cacheKey, "uploadURL", info.URL, "responseBody", string(body))
+		}
 	}
 	if resp.StatusCode == http.StatusOK {
 		// our semantic is that if the object is created, then we return 201
